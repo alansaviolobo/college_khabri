a68e953457e6ed962140c45999eeb04a19547d0d
alansaviolobo <alansaviolobo@5e5f1402-3161-47f1-b9dc-8a507f87b2ec>
2009-04-01 04:07:16 +0000

    adding university model again as it was deleted due to changed filename.
    git-svn-id: https://collegekhabri.unfuddle.com/svn/collegekhabri_portal@5 5e5f1402-3161-47f1-b9dc-8a507f87b2ec

---
+++ trunk/system/application/models/university.php
@@ -0,0 +1,30 @@
+<?php
+class University extends Model
+{
+    function University()
+    {
+        parent::Model();
+    }
+
+    function getUniversity($universityId)
+    {
+        parent::Model();
+
+        $result = $this->db->from('universities')->where('id', $universityId);
+        if ($result->num_rows() <> 1){
+            throw new Exception('Invalid Username or Password');
+        }
+        $this->set($result->row_object());
+        $result->free_result();
+    }
+
+    function getAllUniversities()
+    {
+        $query = $this->db->select('id, name')->get('universities');
+        $result = array();
+        foreach($query->result_array() as $row) $result[$row['id']]=$row['name'];
+        return $result;
+    }
+}
+?>
+
