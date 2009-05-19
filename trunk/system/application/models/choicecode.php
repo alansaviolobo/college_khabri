7cc989087b366778714958aec437b0fa2eeda88b
alansaviolobo <alansaviolobo@5e5f1402-3161-47f1-b9dc-8a507f87b2ec>
2009-05-19 12:58:10 +0000

    BugId: 00 added faculty data to the choice code info tab.
    git-svn-id: https://collegekhabri.unfuddle.com/svn/collegekhabri_portal@157 5e5f1402-3161-47f1-b9dc-8a507f87b2ec

--- trunk/system/application/models/choicecode.php
+++ trunk/system/application/models/choicecode.php
@@ -5,6 +5,16 @@ class Choicecode extends Model
 	private $college;
 	private $course;
 	private $popularity;
+	private $facultySanctionedIntake;
+	private $facultyRequired;
+	private $facultyGraduate;
+	private $facultyPostGraduate;
+	private $facultyDoctorate;
+	private $facultyTeachingExp;
+	private $facultyIndustryExp;
+	private $facultyResearchExp;
+	private $facultyPermanentFacultyStudentRatio;
+	private $facultyFacultyStudentRatio;
 	
     function Choicecode()
     {
@@ -17,11 +27,21 @@ class Choicecode extends Model
     function popularity() {if (is_null($this->popularity)) $this->set(); return $this->popularity; }
     function institute() {if (is_null($this->college)) $this->set(); return $this->college; }
     function course() {if (is_null($this->course)) $this->set(); return $this->course; }
-    
+    function facultySanctionedIntake() {if (is_null($this->facultySanctionedIntake)) $this->set(); return $this->facultySanctionedIntake; }
+	function facultyRequired() {if (is_null($this->facultyRequired)) $this->set(); return $this->facultyRequired; }
+	function facultyGraduate() {if (is_null($this->facultyGraduate)) $this->set(); return $this->facultyGraduate; }
+	function facultyPostGraduate() {if (is_null($this->facultyPostGraduate)) $this->set(); return $this->facultyPostGraduate; }
+	function facultyDoctorate() {if (is_null($this->facultyDoctorate)) $this->set(); return $this->facultyDoctorate; }
+	function facultyTeachingExp() {if (is_null($this->facultyTeachingExp)) $this->set(); return $this->facultyTeachingExp; }
+	function facultyIndustryExp() {if (is_null($this->facultyIndustryExp)) $this->set(); return $this->facultyIndustryExp; }
+	function facultyResearchExp() {if (is_null($this->facultyResearchExp)) $this->set(); return $this->facultyResearchExp; }
+	function facultyPermanentFacultyStudentRatio() {if (is_null($this->facultyPermanentFacultyStudentRatio)) $this->set(); return $this->facultyPermanentFacultyStudentRatio; }
+	function facultyFacultyStudentRatio() {if (is_null($this->facultyFacultyStudentRatio)) $this->set(); return $this->facultyFacultyStudentRatio; }
+
     static function getChoicecode($id)
     {
 		$choicecode = new Choicecode();
-        $result = $choicecode->db->where('code', $id)->get('choice_codes');
+        $result = $choicecode->db->from('choice_codes')->join('faculty', 'choice_codes.code = faculty.choice_code')->where('code', $id)->get();
         if ($result->num_rows() <> 1){
             throw new Exception('Invalid Choice code');
         }
@@ -34,13 +54,23 @@ class Choicecode extends Model
     {
         if (is_null($data))
         {
-            $data = $this->db->where('code', $this->choicecode)->get('choice_codes')->result_object();
+            $data = $this->db->from('choice_codes')->join('faculty', 'choice_codes.code = faculty.choice_code')->where('code', $this->code)->result_object();
         }
 
         $this->choice = $data->code;
         $this->popularity = $data->popularity;
         $this->college = Institute::getInstituteById($data->institute_code);
         $this->course =  Course::getCourse($data->course_code);
+        $this->facultySanctionedIntake = $data->sanctioned_intake;
+		$this->facultyRequired = $data->required_faculty;
+		$this->facultyGraduate = $data->graduate;
+		$this->facultyPostGraduate = $data->post_graduate;
+		$this->facultyDoctorate = $data->doctorate;
+		$this->facultyTeachingExp = $data->teaching_experience;
+		$this->facultyIndustryExp = $data->industry_experience;
+		$this->facultyResearchExp = $data->research_experience;
+		$this->facultyPermanentFacultyStudentRatio = $data->permanent_faculty_student_ratio;
+		$this->facultyFacultyStudentRatio = $data->faculty_student_ratio;
     }    
 }
 ?>
